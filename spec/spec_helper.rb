fcc007f2a2c0afbea27d7c347eada5ddc4da022f
Ty Bui <nhamtybv@hotmail.com>
2016-02-24 08:08:39 +0000

    Update application controller to prevent CSRF exception from railsed.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,8 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
+require 'rspec/autorun'
+require 'email_spec'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -57,5 +59,26 @@ RSpec.configure do |config|
   #
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/v/3-0/docs
+  #config.include(EmailSpec::Helpers)
+  #config.include(EmailSpec::Matchers)
+
   config.infer_spec_type_from_file_location!
+
+  config.include Request::JsonHelpers, :type => :controller
+  config.include Request::HeadersHelpers, :type => :controller
+  config.include Devise::TestHelpers, :type => :controller
+
+  config.include(EmailSpec::Helpers)
+  config.include(EmailSpec::Matchers)
+
+  config.before(:each, type: :controller) do
+    include_default_accept_headers
+  end
+end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
 end
